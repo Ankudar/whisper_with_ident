68f5eb324cea2d062bdba17a517a26530b5f66b5
root6 <root6@corp.ingate.ru>
2023-11-14 11:25:23 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+whisper_faster + speaker_ident
+https://github.com/guillaumekln/faster-whisper
+https://github.com/Ankudar/voice_identify_learn_model
--- bac_all_structure.py
+++ bac_all_structure.py
@@ -5,14 +5,10 @@ from tqdm import tqdm
 import time
 import datetime
 
-# Исходная и конечная директории
-src_dir = r'C:/Users/root6/Desktop/python/'
-dst_dir = r'//192.168.5.212/E$/python/bac_from_ib-rtx/'
+src_dir = r'dir'
+dst_dir = r'dir'
 
-# Список папок для исключения содержимого
-exclude_dirs = ['zoom_api/zoom_recording', 'LLAMA', 
-                'machine_learning/train/data_set', 'machine_learning/train/обработка аудио_СКОПИРОВАТЬ_НЕ_ИСПОЛЬЗОВАТЬ_НАПРЯМУЮ', 
-                'voice_to_text/whisper/input', 'voice_to_text/whisper/models']
+exclude_dirs = ['ignore_this_folder']
 
 def copy_files(src_path, dst_path):
     if not dst_path.exists():
@@ -41,15 +37,12 @@ files_to_copy = get_all_files(Path(src_dir))
 
 pbar = tqdm(total=len(files_to_copy), ncols=100)
 while True:
-    # Создание подпапок с текущей датой
     current_date = time.strftime('%Y/%m/%d')
     dst_dir_new = Path(dst_dir) / current_date
 
-    # Проверка, производилось ли копирование сегодня
     if not dst_dir_new.exists():
         copy_files(Path(src_dir), dst_dir_new)
 
-    # Проверка времени и ожидание следующего дня для повторного копирования
     current_time = datetime.datetime.now()
     next_day = (current_time + datetime.timedelta(days=1)).replace(hour=0, minute=0, second=0)
     time_to_next_day = (next_day - current_time).total_seconds()
